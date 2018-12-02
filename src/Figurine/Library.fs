e4c63aabdc24a9afcc5020e3e5b2c4919e857d39
Chet Husk <chusk3@gmail.com>
2018-12-02 14:46:38 -0600

    initial implementations for read/write/url/find/findMany

--- src/Figurine/Library.fs
+++ src/Figurine/Library.fs
@@ -1,9 +1,96 @@
 namespace Figurine
 
-module Say =
-    let nothing name =
-        name |> ignore
+open System
+open PuppeteerSharp
 
-    let hello name =
-        sprintf "Hello %s" name
+module Task =
+    open System.Threading.Tasks
+
+    let force (t: Task<'t>) = t.Result
+    let forceUnit (t: Task) = t.Wait()
+
+    let withTimeout (taskFn: unit -> Task<'t>) = failwith "implement withTimeout"
+
+module Response =
+    let check (r: Response) = if not r.Ok then failwith "bad response"
+
+module Types =
+    type BrowserCtx = {
+        /// the actual unit of work
+        page: Page
+        dispose : unit -> unit
+        compareTimeout : System.TimeSpan
+        navigateTimeout : System.TimeSpan
+    }
+    with
+        member x.CurrentUrl = x.page.Url
+
+        interface IDisposable with
+            member x.Dispose () = x.dispose ()
+
+module Browser =
+    open Types
+
+    type LaunchMethod =
+    | Launch of LaunchOptions
+    | Connect of ConnectOptions
+
+    let create (method: LaunchMethod) =
+        let factory =
+            match method with
+            | Launch opts -> Puppeteer.LaunchAsync opts
+            | Connect opts -> Puppeteer.ConnectAsync opts
+        let b = Task.force factory
+        let p = b.NewPageAsync() |> Task.force
+
+        { page = p
+          dispose = b.Dispose
+          compareTimeout = TimeSpan.FromSeconds 10.
+          navigateTimeout = TimeSpan.FromSeconds 30. }
+
+    let createFromExe (path: string) =
+        create (Launch (LaunchOptions(ExecutablePath = path, Headless = false)))
+
+module Functions =
+    open Types
+    open Newtonsoft.Json.Linq
+
+    let (|JsonString|_|) (desired: string) (expected: JToken) =
+        if expected.Type = JTokenType.String && string expected = desired
+        then Some ()
+        else None
+
+    let js script (d: BrowserCtx) = failwith "boom"
+    let puts text (d: BrowserCtx) = failwith "boom"
+    let waitFor condition (d: BrowserCtx) = failwith "boom"
+
+    let findSelector selector (d: BrowserCtx) =
+        d.page.QuerySelectorAsync selector // todo: timeout
+        |> Task.force
+        |> Option.ofObj
+
+    let findManySelector selector (d: BrowserCtx) =
+        d.page.QuerySelectorAllAsync selector // todo: timeout
+        |> Task.force
+
+    let url u (d: BrowserCtx) =
+        d.page.GoToAsync u
+        |> Task.force
+        |> Response.check
+
+    let write (text: string) (element: ElementHandle) =
+        element.TypeAsync(text) |> Task.forceUnit
+
+    let read (element: ElementHandle) (d: BrowserCtx) =
+        match element.RemoteObject.["className"] with
+        | JsonString "HtmlInputElement"
+        | JsonString "HtmlTextAreaElement" ->
+            element.GetPropertyAsync("value")
+            |> Task.force
+            |> fun js -> string js.RemoteObject
+
+        // TODO: implement select text get here
+        | _ -> element.GetPropertyAsync("innerText")
+               |> Task.force
+               |> fun js -> string js.RemoteObject
 
--- tests/Figurine.Tests/Tests.fs
+++ tests/Figurine.Tests/Tests.fs
@@ -3,14 +3,45 @@ module Tests
 
 open Expecto
 open Figurine
+open Figurine.Functions
+open PuppeteerSharp
+
+let chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+let googleHomePage = "https://www.google.com/"
 
 [<Tests>]
 let tests =
   testList "samples" [
-    testCase "Say nothing" <| fun _ ->
-      let subject = Say.nothing ()
-      Expect.equal subject () "Not an absolute unit"
-    testCase "Say hello all" <| fun _ ->
-      let subject = Say.hello "all"
-      Expect.equal subject "Hello all" "You didn't say hello"
+    testCase "Go to google" <| fun () ->
+
+      let ctx = Browser.createFromExe chromePath
+
+      url googleHomePage ctx
+
+      Expect.equal ctx.CurrentUrl googleHomePage "should have gone to google"
+
+    // pending because the input element on google's search page doesn't show the typed value in the innerText
+    ptestCase "Can manipulate element via selector" <| fun () ->
+        let ctx = Browser.createFromExe chromePath
+
+        url googleHomePage ctx
+
+        let element = findSelector "input[aria-label=Search]" ctx
+        Expect.isSome element "should have found the search input"
+
+        let greeting = "Hello from Figurine!"
+        write greeting element.Value
+
+        let written = read element.Value ctx
+
+        Expect.equal written greeting "should have been able to round-trip the greeting"
+
+    testCase "Can get all elements and print them" <| fun () ->
+        let ctx = Browser.createFromExe chromePath
+        url googleHomePage ctx
+
+        let elements = findManySelector "*" ctx
+
+        Expect.isGreaterThan elements.Length 0 "should have found some elements on the page"
+
   ]
