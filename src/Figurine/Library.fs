af2094a19e8a05ec69a3d70500dfc25c75c97c1d
Chet Husk <chusk3@gmail.com>
2018-12-08 15:44:41 -0600

    fix property reads and apply timeouts

--- src/Figurine/Library.fs
+++ src/Figurine/Library.fs
@@ -3,13 +3,30 @@ namespace Figurine
 open System
 open PuppeteerSharp
 
+[<RequireQualifiedAccess>]
 module Task =
     open System.Threading.Tasks
+    open System.Threading
 
     let force (t: Task<'t>) = t.Result
     let forceUnit (t: Task) = t.Wait()
 
-    let withTimeout (taskFn: unit -> Task<'t>) = failwith "implement withTimeout"
+    let withTimeout (timeSpan: System.TimeSpan) (taskFn: unit -> Task<'t>) =
+        async {
+            let tcs = new TaskCompletionSource<bool>()
+            let cts = new CancellationTokenSource(timeSpan)
+            use reg = cts.Token.Register(System.Action<obj>(fun s -> (s :?> TaskCompletionSource<bool>).TrySetResult(true) |> ignore), tcs)
+            let t = taskFn ()
+            let! first = Task.WhenAny(t :> Task, tcs.Task) |> Async.AwaitTask
+            if not <| first.Equals(t) then
+                return failwithf "could not complete operation in %f seconds" timeSpan.TotalSeconds
+            else
+                return! Async.AwaitTask t
+        }
+        |> Async.StartAsTask
+
+    let withTimeoutUnit (timeSpan: System.TimeSpan) (taskFn: unit -> Task) =
+        withTimeout timeSpan (fun () -> taskFn().ContinueWith<unit>(fun t -> ()))
 
 module Response =
     let check (r: Response) = if not r.Ok then failwith "bad response"
@@ -27,6 +44,26 @@ module Types =
 
         interface IDisposable with
             member x.Dispose () = x.dispose ()
+
+module Handle =
+    let className (handle: ElementHandle) =
+        handle.RemoteObject.["className"]
+
+    type PropertyResult = {``type``: string; ``value``: string }
+
+    let property propName (ctx: Types.BrowserCtx) (handle: ElementHandle) =
+        fun () -> handle.GetPropertyAsync(propName)
+        |> Task.withTimeout (ctx.compareTimeout)
+        |> Task.force
+        |> fun js ->
+            let jobj = js.RemoteObject :?> Newtonsoft.Json.Linq.JObject
+            let typed = jobj.ToObject<PropertyResult>()
+            typed.value
+
+    let children selector (ctx: Types.BrowserCtx) (handle: ElementHandle) =
+        fun () -> handle.QuerySelectorAllAsync selector
+        |> Task.withTimeout ctx.compareTimeout
+        |> Task.force
 
 module Browser =
     open Types
@@ -49,7 +86,7 @@ module Browser =
           navigateTimeout = TimeSpan.FromSeconds 30. }
 
     let createFromExe (path: string) =
-        create (Launch (LaunchOptions(ExecutablePath = path)))
+        create (Launch (LaunchOptions(ExecutablePath = path, Headless = false)))
 
 module Functions =
     open Types
@@ -65,32 +102,37 @@ module Functions =
     let waitFor condition (d: BrowserCtx) = failwith "boom"
 
     let findSelector selector (d: BrowserCtx) =
-        d.page.QuerySelectorAsync selector // todo: timeout
+        fun () -> d.page.QuerySelectorAsync selector
+        |> Task.withTimeout d.compareTimeout
         |> Task.force
         |> Option.ofObj
 
     let findManySelector selector (d: BrowserCtx) =
-        d.page.QuerySelectorAllAsync selector // todo: timeout
+        fun () -> d.page.QuerySelectorAllAsync selector
+        |> Task.withTimeout d.compareTimeout
         |> Task.force
 
     let url u (d: BrowserCtx) =
-        d.page.GoToAsync u
+        fun () -> d.page.GoToAsync u
+        |> Task.withTimeout d.navigateTimeout
         |> Task.force
         |> Response.check
 
-    let write (text: string) (element: ElementHandle) =
-        element.TypeAsync(text) |> Task.forceUnit
+    let write (text: string) (element: ElementHandle) (d: BrowserCtx) =
+        fun () -> element.TypeAsync(text)
+        |> Task.withTimeoutUnit d.compareTimeout
+        |> Task.force
 
     let read (element: ElementHandle) (d: BrowserCtx) =
-        match element.RemoteObject.["className"] with
-        | JsonString "HtmlInputElement"
-        | JsonString "HtmlTextAreaElement" ->
-            element.GetPropertyAsync("value")
-            |> Task.force
-            |> fun js -> string js.RemoteObject
-
-        // TODO: implement select text get here
-        | _ -> element.GetPropertyAsync("innerText")
-               |> Task.force
-               |> fun js -> string js.RemoteObject
+        match Handle.className element with
+        | JsonString "HTMLInputElement"
+        | JsonString "HTMLTextAreaElement" ->
+            Handle.property "value" d element
+        | JsonString "HTMLSelectElement" ->
+            let currentValue = Handle.property "value" d element
+            let options = Handle.children "option" d element
+            let selectedOption = options |> Array.find (fun option -> Handle.property "value" d option = currentValue)
+            Handle.property "innerText" d selectedOption
+        | _ ->
+            Handle.property "innerText" d element
 
--- tests/Figurine.Tests/Tests.fs
+++ tests/Figurine.Tests/Tests.fs
@@ -4,6 +4,7 @@ module Tests
 open Expecto
 open Figurine
 open Figurine.Functions
+open Newtonsoft.Json.Linq
 open PuppeteerSharp
 
 let chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
@@ -14,30 +15,31 @@ let tests =
   testList "samples" [
     testCase "Go to google" <| fun () ->
 
-      let ctx = Browser.createFromExe chromePath
+      use ctx = Browser.createFromExe chromePath
 
       url googleHomePage ctx
 
       Expect.equal ctx.CurrentUrl googleHomePage "should have gone to google"
 
     // pending because the input element on google's search page doesn't show the typed value in the innerText
-    ptestCase "Can manipulate element via selector" <| fun () ->
-        let ctx = Browser.createFromExe chromePath
+    testCase "Can manipulate element via selector" <| fun () ->
+        use ctx = Browser.createFromExe chromePath
+        let wikipedia = "https://www.wikipedia.org"
 
-        url googleHomePage ctx
+        url wikipedia ctx
 
-        let element = findSelector "input[aria-label=Search]" ctx
+        let element = findSelector "input[name=search]" ctx
         Expect.isSome element "should have found the search input"
 
         let greeting = "Hello from Figurine!"
-        write greeting element.Value
+        write greeting element.Value ctx
 
         let written = read element.Value ctx
 
         Expect.equal written greeting "should have been able to round-trip the greeting"
 
     testCase "Can get all elements and print them" <| fun () ->
-        let ctx = Browser.createFromExe chromePath
+        use ctx = Browser.createFromExe chromePath
         url googleHomePage ctx
 
         let elements = findManySelector "*" ctx
